fa98c6b3ed14e3c4a0845ccb82dfff94b92ca382
devin <roche.devin@ymail.com>
2017-07-13 16:46:01 -0700

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scheduleBuilderApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scheduleBuilderApp
+ */
+angular.module('scheduleBuilderApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scheduleBuilderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scheduleBuilderApp
+ */
+angular.module('scheduleBuilderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
