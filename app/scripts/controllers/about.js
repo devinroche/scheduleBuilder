b20e107adb4eb947fd187f26b6e824689dc206d1
devin <roche.devin@ymail.com>
2017-07-15 17:12:43 -0700

    working generator
    need to add styles now

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * @ngdoc function
@@ -7,42 +7,72 @@
  * # AboutCtrl
  * Controller of the scheduleBuilderApp
  */
-angular.module('scheduleBuilderApp')
-  .controller('AboutCtrl', function ($scope, $http) {
+angular
+  .module("scheduleBuilderApp")
+  .controller("AboutCtrl", function($scope, $http) {
     $scope.searchObj = "";
     $scope.resultArr = [];
     $scope.allClasses = [];
 
-    $scope.submitSearch = function (searchParams) {
+    $scope.submitSearch = function(searchParams) {
       let searchFormat = "?courses=" + searchParams;
       $http({
-        method: 'GET',
-        url: 'https://schedule-builder-backend.herokuapp.com/api/schedules/' + searchFormat
-      }).then(function successCallback(response) {
-        $scope.resultArr = response.data;
-        console.log($scope.response.data);
-      }, function errorCallback(response) {
-        console.log(response);
-      });
+        method: "GET",
+        url:
+          "https://schedule-builder-backend.herokuapp.com/api/schedules/" +
+          searchFormat
+      }).then(
+        function successCallback(response) {
+          $scope.resultArr = response.data;
+          console.log($scope.response.data);
+        },
+        function errorCallback(response) {
+          console.log(response);
+        }
+      );
     };
     $http({
-      method: 'GET',
-      url: 'http://schedule-builder-backend.herokuapp.com/api/classes'
-    }).then(function successCallback(response) {
-      $scope.allClasses = response.data;
-      console.log($scope.allClasses);
-    }, function errorCallback(response) {
-      console.log(response);
-    });
+      method: "GET",
+      url: "http://schedule-builder-backend.herokuapp.com/api/classes"
+    }).then(
+      function successCallback(response) {
+        $scope.allClasses = response.data;
+        console.log($scope.allClasses);
+      },
+      function errorCallback(response) {
+        console.log(response);
+      }
+    );
 
     $scope.userClasses = [];
-    $scope.cleanClasses = []
-    $scope.addUserClasses = function (val) {
+    $scope.addUserClasses = function(val) {
       $scope.userClasses.push(val);
-      console.log($scope.userClasses)
-      angular.forEach($scope.userClasses, function (Title, Course) {
-        $scope.cleanClasses.push(Title.Title);
-        console.log($scope.cleanClasses)
-      })
-    }
+    };
+
+    $scope.viableSchedules = [];
+    $scope.preReqClasses = [];
+    $scope.generateSchedule = function(classArr) {
+      for (var i = 0; i < classArr.length; i++) {
+        $scope.preReqClasses.push(
+          classArr[i].originalObject.Subject +
+            " " +
+            classArr[i].originalObject.Course
+        );
+      }
+      $http({
+        method: "POST",
+        data: { classes: $scope.preReqClasses, block: [] },
+        url: "https://schedule-builder-backend.herokuapp.com/API/schedules"
+      }).then(
+        function successCallback(response) {
+          console.log("success")
+          $scope.viableSchedules = response.data;
+          console.log($scope.viableSchedules)
+      },
+      function errorCallback(response) {
+        console.log("fail")
+        console.log(response);
+      }
+      );
+    };
   });
