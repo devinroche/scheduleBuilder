2027a755a2744cb0436865193a2c774d7246cbb0
Devin Roche <Roche.devin@ymail.com>
2017-08-27 11:51:45 -0700

    dist folder

---
+++ web.js
@@ -0,0 +1,7 @@
+const gzippo = require('gzippo')
+const express = require('express');
+const app = express();
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
