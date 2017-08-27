7da260b080d1169d5bf35a61ebd58406299a92a7
Devin Roche <Roche.devin@ymail.com>
2017-08-27 12:11:56 -0700

    v2

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
 const gzippo = require('gzippo')
 const express = require('express');
+const http = require('http')
 const app = express();
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+var app = express()
+app.use(gzippo.staticGzip(''+__dirname))
+app.use('/*', function(req, res){
+    res.sendfile(__dirname+'/index.html')
+})
+
+var server = http.createServer(app)
+server.listenerCount(process.env.PORT || 5000)
