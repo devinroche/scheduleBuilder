891d21f6a7fac615270b8c51dc06856751b698d5
devin <roche.devin@ymail.com>
2017-09-06 11:49:43 -0700

    gzip error

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ const logger = require('morgan')
 const app = express();
 
 app.use(logger());
-app.use(gzippo.staticGzip('' + __dirname));
+app.use(gzippo.staticGzip('' + __dirname + "/dist"));
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 5000);
