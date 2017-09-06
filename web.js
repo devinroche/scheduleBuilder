754f5c2bb30c84ea6471006421bc77edd5275878
devin <roche.devin@ymail.com>
2017-09-06 11:48:03 -0700

    deploying stinx

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 const gzippo = require('gzippo')
 const express = require('express');
 const http = require('http')
+const logger = require('morgan')
+
 const app = express();
 
+app.use(logger());
 app.use(gzippo.staticGzip('' + __dirname));
-app.use('/*', function(req, res){
-  res.sendfile(__dirname + '/index.html');
-});
 
 var server = http.createServer(app);
 server.listen(process.env.PORT || 5000);
