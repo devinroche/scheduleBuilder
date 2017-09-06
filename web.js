f1cec337e9a83545a51d2584fd90b23b875c122b
devin <roche.devin@ymail.com>
2017-09-06 11:35:36 -0700

    heroku app error

--- web.js
+++ web.js
@@ -3,6 +3,10 @@ const express = require('express');
 const http = require('http')
 const app = express();
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.use(gzippo.staticGzip('' + __dirname));
+app.use('/*', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+var server = http.createServer(app);
+server.listen(process.env.PORT || 5000);
