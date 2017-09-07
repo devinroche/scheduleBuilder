dc692e7b04442ed059b604eb59db3a0ff7bd30d5
Devin Roche <Roche.devin@ymail.com>
2017-09-06 21:39:55 -0700

    test

--- web.js
+++ web.js
@@ -2,17 +2,8 @@ const gzippo = require('gzippo')
 const express = require('express');
 const http = require('http')
 const logger = require('morgan')
-const csp = require('helmet-csp')
 
 const app = express();
-
-app.use(csp({
-    directives: {
-        defaultSrc: [`'self'`, 'https://schedule-builder-backend.herokuapp.com/api/'],
-        imgSrc: ['imgur.com', 'https://thumb.ibb.co'],
-        styleSrc: ['https://maxcdn.bootstrapcdn.com', 'https://cdnjs.cloudflare.com']
-    }
-}))
 
 app.use(logger());
 app.use(gzippo.staticGzip('' + __dirname + "/dist"));
